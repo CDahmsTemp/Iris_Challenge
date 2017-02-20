055a85f68a49644930620df046edbfc2fbb24572
MicrocontrollersAndMore <cdahms123@gmail.com>
2017-02-20 08:08:23 -0500

    still setting up multiple files

---
+++ UtilityFunctions.cpp
@@ -0,0 +1,11 @@
+// UtilityFunctions.cpp
+
+#include "UtilityFunctions.h"
+
+
+
+
+
+
+
+
---
+++ UtilityFunctions.h
@@ -0,0 +1,20 @@
+// UtilityFunctions.h
+
+#ifndef UTILITY_FUNCTIONS
+#define UTILITY_FUNCTIONS
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+// function prototypes ////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+#endif	// UTILITY_FUNCTIONS
